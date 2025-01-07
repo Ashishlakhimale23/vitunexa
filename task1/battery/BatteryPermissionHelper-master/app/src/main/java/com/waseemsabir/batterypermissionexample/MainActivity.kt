6947b14b6f321fb4eac6e99d56b6f5c27e6d7899
LeelanshNikumbh <155939396+LeelanshNikumbh@users.noreply.github.com>
2025-01-07 12:57:23 +0530

    Add files via upload

---
+++ task1/battery/BatteryPermissionHelper-master/app/src/main/java/com/waseemsabir/batterypermissionexample/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.waseemsabir.batterypermissionexample
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.waseemsabir.batterypermissionexample.databinding.ActivityMainBinding
+import com.waseemsabir.betterypermissionhelper.BatteryPermissionHelper
+
+class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+  private val batteryPermissionHelper = BatteryPermissionHelper.getInstance()
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    val isBatteryPermissionAvailable =
+        batteryPermissionHelper.isBatterySaverPermissionAvailable(this, true)
+
+    binding.permissionTextView.text =
+        if (isBatteryPermissionAvailable) {
+          "Battery Permission Available for this Device."
+        } else {
+          "No Battery Permission Available for this Device."
+        }
+    binding.permissionTextView.visibility = View.VISIBLE
+    binding.openButton.visibility = if (isBatteryPermissionAvailable) View.VISIBLE else View.GONE
+    binding.openButton.setOnClickListener {
+      batteryPermissionHelper.getPermission(this, open = true, newTask = true)
+    }
+  }
+}
